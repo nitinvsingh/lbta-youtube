9006762a8c9316ca722e96e2d258a995adf50d26
Nitin Singh <nsingh@cogitate.us>
2018-11-08 17:07:06 +0530

    [18] Finished lesson 17.

--- Youtube/Helpers/VideoLauncher.swift
+++ Youtube/Helpers/VideoLauncher.swift
@@ -10,21 +10,89 @@ import UIKit
 import AVFoundation
 
 class VideoPlayer: UIView {
+    
+    var avPlayer: AVPlayer?
+    
+    let controlContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+        return view
+    }()
+    
+    let spinner: UIActivityIndicatorView = {
+        let _spinner = UIActivityIndicatorView(style: .white)
+        _spinner.translatesAutoresizingMaskIntoConstraints = false
+        return _spinner
+    }()
+    
+    lazy var playPause: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        let image = UIImage(named: "pause")
+        button.setImage(image, for: .normal)
+        button.addTarget(self, action: #selector(togglePlayPause), for: .touchUpInside)
+        button.isHidden = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var isPlaying = false
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
         
-        if let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726") {
-            let avPlayer = AVPlayer(url: url)
-            let playerLayer = AVPlayerLayer(player: avPlayer)
-            self.layer.addSublayer(playerLayer)
-            playerLayer.frame = self.frame
-            avPlayer.play()
-        }
+        setupPlayerView()
+        
+        controlContainer.frame = frame
+        addSubview(controlContainer)
+        controlContainer.addSubview(spinner)
+        controlContainer.addSubview(playPause)
+        
+        spinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        playPause.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        playPause.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        playPause.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        playPause.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        spinner.startAnimating()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupPlayerView() {
+        if let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726") {
+            avPlayer = AVPlayer(url: url)
+            let playerLayer = AVPlayerLayer(player: avPlayer)
+            self.layer.addSublayer(playerLayer)
+            playerLayer.frame = self.frame
+            avPlayer?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+            avPlayer?.play()
+        }
+    }
+    
+    @objc func togglePlayPause() {
+        if isPlaying {
+            avPlayer?.pause()
+            playPause.setImage(UIImage(named: "play"), for: .normal)
+        } else {
+            avPlayer?.play()
+            playPause.setImage(UIImage(named: "pause"), for: .normal)
+        }
+        isPlaying = !isPlaying
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            spinner.stopAnimating()
+            controlContainer.backgroundColor = .clear
+            playPause.isHidden = false
+            isPlaying = true
+        }
     }
 }
 
@@ -39,7 +107,7 @@ class VideoLauncher: NSObject {
             
             // VideoPlayer view
             let height = keyWindow.frame.width * 9 / 16
-            let videoFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.height, height: height)
+            let videoFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
             let videoPlayer = VideoPlayer(frame: videoFrame)
             
             view.addSubview(videoPlayer)
